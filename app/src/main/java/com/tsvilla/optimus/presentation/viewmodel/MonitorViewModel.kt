975d39f48889be7e131ba9fceaa560101f1a4492
Keny <sainthilaire.keny@airweb.fr>
2022-05-28 00:07:20 +0200

    :bug: fix timeMillis

--- app/src/main/java/com/tsvilla/optimus/presentation/viewmodel/MonitorViewModel.kt
+++ app/src/main/java/com/tsvilla/optimus/presentation/viewmodel/MonitorViewModel.kt
@@ -27,19 +27,26 @@ import org.koin.java.KoinJavaComponent.inject
 class MonitorViewModel : SensorEventListener, BaseViewModel<MonitorState>(
     MonitorState(isPaused = false)
 ) {
+
+    companion object {
+        const val TAG = "MonitorViewModel"
+    }
+
     private val repository: ITsvillaRepository by inject(TsvillaRepository::class.java)
     private val sensorManager: SensorManager by inject(SensorManager::class.java)
 
-    private val timeMillis: Long = 500 // Desired delay between events in microseconds
+    private val timeMillis: Long = 500// Desired delay between events in microseconds
+    private val timeBeforeSending: Long = 10000
 
     init {
-        generateAndSendRandomBpm()
-        //initSensor()
+        //generateAndSendRandomBpm()
+        initSensor()
     }
 
     private fun initSensor() {
         val heartSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE)
         sensorManager.registerListener(this, heartSensor, timeMillis.toInt())
+        sendBpm()
     }
 
 
@@ -66,21 +73,26 @@ class MonitorViewModel : SensorEventListener, BaseViewModel<MonitorState>(
     fun currentBPM(): Int = if (_state.value != null) _state.value!!.currentBPM else 0
 
     fun switchSending() {
-        Log.d("TAG", "state ${_state.value.toString()}")
         val state = MonitorState(currentBPM = currentBPM(), isPaused = !isPaused())
 
         _state.onNext(state)
     }
 
 
-    private fun sendBpm(bpm: Int) {
-        val disposable = repository.sendBPM(bpm)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { _ ->
-                val state = MonitorState(currentBPM = bpm)
-                _state.onNext(state)
-            }
+    private fun sendBpm() {
+        viewModelScope.launch {
+            delay(timeBeforeSending)
+            if (isPaused()) return@launch sendBpm()
+            val currentState = _state.value ?: return@launch sendBpm()
+            val bpm = currentState!!.currentBPM
+            repository.sendBPM(bpm)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ _ ->
+                    val state = MonitorState(currentBPM = bpm)
+                    _state.onNext(state)
+                }, { tr -> Log.d(TAG, tr.toString()) })
+        }
     }
 
     override fun onSensorChanged(event: SensorEvent) {
@@ -90,7 +102,6 @@ class MonitorViewModel : SensorEventListener, BaseViewModel<MonitorState>(
                 val bpm = event.values[0].toInt()
                 val state = MonitorState(currentBPM = bpm)
                 _state.onNext(state)
-                //     sendBpm(bpm.toInt())
             }
         }
     }
